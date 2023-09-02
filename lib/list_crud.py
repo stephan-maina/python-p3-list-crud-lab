fc90a6442338c6a0abe62f0e4fcb0ac0f07e537e
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-02 03:02:30 +0300

    Update list_crud.py

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,63 @@
 def create_an_empty_list():
-    return None
+    """Create and return an empty list."""
+    return []
 
-def create_a_list():
-    return None
 
-def add_element_to_end_of_list(l, element):
-    return None
+def create_a_list(elements=None):
+    """Create and return a list with optional initial elements."""
+    if elements is None:
+        elements = []
+    return list(elements)
 
-def add_element_to_start_of_list(l, element):
-    return None
 
-def remove_element_from_end_of_list(l):
-    return None
+def add_element_to_end_of_list(lst, element):
+    """Add an element to the end of the list and return the modified list."""
+    lst.append(element)
+    return lst
 
-def remove_element_from_start_of_list(l):
-    return None
 
-def retrieve_first_element_from_list(l):
-    return None
+def add_element_to_start_of_list(lst, element):
+    """Add an element to the start of the list and return the modified list."""
+    lst.insert(0, element)
+    return lst
 
-def retrieve_element_from_index(l, index):
-    return None
 
-def retrieve_last_element_from_list(l):
-    return None
+def remove_element_from_end_of_list(lst):
+    """Remove and return the last element from the list."""
+    if lst:
+        return lst.pop()
+    else:
+        raise IndexError("Cannot remove from an empty list")
+
+
+def remove_element_from_start_of_list(lst):
+    """Remove and return the first element from the list."""
+    if lst:
+        return lst.pop(0)
+    else:
+        raise IndexError("Cannot remove from an empty list")
+
+
+def retrieve_first_element_from_list(lst):
+    """Retrieve and return the first element from the list."""
+    if lst:
+        return lst[0]
+    else:
+        raise IndexError("List is empty")
+
+
+def retrieve_element_from_index(lst, index):
+    """Retrieve and return an element from the list by index."""
+    if 0 <= index < len(lst):
+        return lst[index]
+    else:
+        raise IndexError("Index out of range")
+
+
+def retrieve_last_element_from_list(lst):
+    """Retrieve and return the last element from the list."""
+    if lst:
+        return lst[-1]
+    else:
+        raise IndexError("List is empty")
+
