c7ca6b40a34ae6a6a2959c8701cf8278404ec145
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-02 03:01:39 +0300

    Update debug.py

--- lib/debug.py
+++ lib/debug.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+"""
+This script demonstrates basic list manipulation operations using functions from the list_crud module.
+"""
+
 from list_crud import (
     create_an_empty_list, create_a_list,
     add_element_to_end_of_list, add_element_to_start_of_list,
@@ -9,4 +13,13 @@ from list_crud import (
 )
 
 if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+   from list_crud import (
+    create_an_empty_list, create_a_list,
+    add_element_to_end_of_list, add_element_to_start_of_list,
+    remove_element_from_end_of_list, remove_element_from_start_of_list,
+    retrieve_first_element_from_list, retrieve_element_from_index,
+    retrieve_last_element_from_list
+)
+
+if __name__ == '__main__':
+    import ipdb; ipdb.set_trace() ; ipdb.set_trace()
